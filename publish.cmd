927adabd3b913729717167af47dc00858ed95824
Razeral <ng.shangru@gmail.com>
2018-02-23 15:05:47 +0800

    1

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=askeugene-Web-Deploy.pubxml -p:Password=5kl0xXjfeQ2bYiWnm1GPBmbmqHd1gHD58q0lkm0iuhfEvr15yKj8mirnqSne
+
