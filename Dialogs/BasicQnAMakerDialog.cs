4ee082dbe0a51c4041bbba85b321f46f9ee9cca3
Ng Shangru <ng.shangru@gmail.com>
2018-03-06 13:51:10 +0800

    1

--- Dialogs/BasicQnAMakerDialog.cs
+++ Dialogs/BasicQnAMakerDialog.cs
@@ -31,125 +31,108 @@ namespace Microsoft.Bot.Sample.QnABot
         public string longName { get; set; }
         public string description { get; set; }
     }
+    public class ConnectToTableStorage
+    {
+        public static CloudTable Connect(string TableName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                    CloudConfigurationManager.GetSetting("TableStorageConnString"));
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            return tableClient.GetTableReference(TableName);
+        }
+    }
+    
+
 
     [Serializable]
     public class RootDialog : IDialog<object>
     {
         public async Task StartAsync(IDialogContext context)
         {
-            /* Wait until the first message is received from the conversation and call MessageReceviedAsync 
-            *  to process that message. */
             context.Wait(this.MessageReceivedAsync);
         }
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
-            /* When MessageReceivedAsync is called, it's passed an IAwaitable<IMessageActivity>. To get the message,
-            *  await the result. */
             var message = await result;
-            System.Diagnostics.Trace.TraceInformation("Message received - " + message.Text);
-            string json = JsonConvert.SerializeObject(message, Formatting.Indented);
-            System.Diagnostics.Trace.TraceInformation("<<<MESSAGE>>> - " + json);
+            //System.Diagnostics.Trace.TraceInformation("Message received - " + message.Text);
+            //string json = JsonConvert.SerializeObject(message, Formatting.Indented);
+            //System.Diagnostics.Trace.TraceInformation("<<<MESSAGE>>> - " + json);
 
             var msg = "";
+            var isFirstWordTeach = (message.Text.Split(' '))[0].ToLower() == "teach";
+            var wordCount = message.Text.Split(' ');
 
-            //System.Diagnostics.Trace.TraceInformation(">>0>> " + (message.Text.Split(' '))[0]);
-            //System.Diagnostics.Trace.TraceInformation(">>1>> " + (message.Text.Split(' '))[1]);
-
-            if ((message.Text.Split(' '))[0] == "teach" && (message.Text.Split(' ')).Length >= 2)
+            if (isFirstWordTeach)
             {
-                try
+                if (wordCount <= 1)
                 {
-                    System.Diagnostics.Trace.TraceInformation((message.Text.Split(' '))[0] + " new word " + (message.Text.Split(' '))[1]);
-                    var newAcronym = message.Text.Substring(("teach").Length + 1).Trim();
+                    await context.PostAsync("Please say \"teach\" followed by the word you want me to learn (e.g. teach ABC)");
+                    context.Wait(MessageReceivedAsync);
+                }
+                else
+                {
+                    var newAcronym = message.Text.Substring(("teach").Length + 1).Trim().ToUpper();
 
                     await context.PostAsync("What does " + newAcronym + " mean?");
                     context.UserData.SetValue("newAcronym", newAcronym);
-
                     context.Wait(TeachAcronymAsync);
-                }
-                catch (Exception e)
-                {
-                    System.Diagnostics.Trace.TraceInformation(e.Message);
-                    context.Wait(MessageReceivedAsync);
                 }
             }
             else
             {
                 try
                 {
-                    //await context.PostAsync("Your query is " + message.ChannelData.query);
-
-                    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                    /*CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
                     CloudConfigurationManager.GetSetting("TableStorageConnString"));
-
-                    // Create the table client.
                     CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-
-                    // Create the CloudTable object that represents the "people" table.
-                    CloudTable table = tableClient.GetTableReference("acronyms");
-
-                    // Create a new customer entity.
-                    //TableEntity customer1 = new TableEntity("JTC", "Test2");
+                    CloudTable table = tableClient.GetTableReference("acronyms");*/
+                    CloudTable table = ConnectToTableStorage.Connect("acronyms");
 
                     TableQuery<AcronymEntity> query = new TableQuery<AcronymEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "JTC"));
                     foreach (AcronymEntity entity in table.ExecuteQuery(query))
                     {
                         if (entity.RowKey.ToUpper() == message.Text.Trim().ToUpper())
-                            msg += " " + entity.longName;
+                        {
+                            if (msg == "")
+                                msg += entity.longName;
+                        }
+                        else
+                            msg += " or " + entity.longName;
                     }
-                    System.Diagnostics.Trace.TraceInformation("5");
+
                     if (msg.Length > 0)
-                    {
-                        await context.PostAsync("From what I know..." + message.Text + " = " + msg);
-                    }
+                        await context.PostAsync("From what I know... " + message.Text + " = " + msg);
                     else
-                    {
                         await context.PostAsync("I didn't understand that! :D Ask me an acronym (e.g. JTC) or say \"teach\" followed by a new word to help me learn! (e.g. teach JTC)");
-                    }
                 }
                 catch (Exception e)
                 {
-                    System.Diagnostics.Trace.TraceInformation(e.Message);
-                    await context.PostAsync("There was an error");
+                    System.Diagnostics.Trace.TraceError(e.Message);
+                    await context.PostAsync("Sorry, there was an error [12]");
                 }
                 context.Wait(MessageReceivedAsync);
             }
-
-            // Create the TableOperation object that inserts the customer entity.
-            //TableOperation insertOperation = TableOperation.Insert(customer1);
-
-            // Execute the insert operation.
-            //table.Execute(insertOperation);
-
-
+            
             /*var qnaSubscriptionKey = Utils.GetAppSetting("QnASubscriptionKey");
             var qnaKBId = Utils.GetAppSetting("QnAKnowledgebaseId");
 
             // QnA Subscription Key and KnowledgeBase Id null verification
             if (!string.IsNullOrEmpty(qnaSubscriptionKey) && !string.IsNullOrEmpty(qnaKBId))
-            {
                 await context.Forward(new BasicQnAMakerDialog(), AfterAnswerAsync, message, CancellationToken.None);
-            }
             else
-            {
                 await context.PostAsync("Please set QnAKnowledgebaseId and QnASubscriptionKey in App Settings. Get them at https://qnamaker.ai.");
-            }*/
-
-
+            */
         }
 
         private async Task AfterAnswerAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
-            // wait for the next user message
             context.Wait(MessageReceivedAsync);
         }
 
         private async Task TeachAcronymAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
-            System.Diagnostics.Trace.TraceInformation("MSG IN TeachAcronymAsync -> Before result");
             var message = await result;
-            System.Diagnostics.Trace.TraceInformation("MSG IN TeachAcronymAsync -> " + message.Text);
             var newAcronym = "";
             context.UserData.TryGetValue("newAcronym", out newAcronym);
             context.UserData.SetValue("newAcronymMeaning", message.Text);
@@ -164,9 +147,7 @@ namespace Microsoft.Bot.Sample.QnABot
 
         private async Task StoreAcronymAsync(IDialogContext context, IAwaitable<bool> result)
         {
-            System.Diagnostics.Trace.TraceInformation("MSG IN StoreAcronymAsync -> Before result");
             var confirm = await result;
-
             if (confirm)
             {
                 await context.PostAsync("What's the password?");
@@ -181,10 +162,7 @@ namespace Microsoft.Bot.Sample.QnABot
 
         private async Task StoreConfirmedAcronymAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
-            System.Diagnostics.Trace.TraceInformation("MSG IN StoreConfirmedAcronymAsync -> Before result");
             var message = await result;
-            System.Diagnostics.Trace.TraceInformation("MSG IN StoreConfirmedAcronymAsync -> " + message.Text);
-
             if (message.Text.ToLower() == "jtcivsd1")
             {
                 try
@@ -194,40 +172,21 @@ namespace Microsoft.Bot.Sample.QnABot
                     
                     context.UserData.TryGetValue("newAcronym", out newAcronym);
                     context.UserData.TryGetValue("newAcronymMeaning", out newAcronymMeaning);
-                    System.Diagnostics.Trace.TraceInformation("MSG IN StoreConfirmedAcronymAsync -> " + newAcronym + " ||| " + newAcronymMeaning);
 
                     CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
                         CloudConfigurationManager.GetSetting("TableStorageConnString"));
 
-                    // Create the table client.
                     CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
 
-                    // Create the CloudTable object that represents the "people" table.
                     CloudTable table = tableClient.GetTableReference("acronyms");
 
-                    // Create a new customer entity.
                     AcronymEntity record = new AcronymEntity("JTC", newAcronym);
                     record.longName = newAcronymMeaning;
                     record.description = "-";
-                    //record.Timestamp = 
-                    //TableEntity record = new TableEntity("JTC", "WHATISWRONG");
 
-                    // Create the TableOperation object that inserts the customer entity.
                     TableOperation insertOperation = TableOperation.InsertOrMerge(record);
 
-                    System.Diagnostics.Trace.TraceInformation("123");
-
-
-                    System.Diagnostics.Trace.TraceInformation(record.PartitionKey);
-                    System.Diagnostics.Trace.TraceInformation(record.RowKey);
-                    System.Diagnostics.Trace.TraceInformation(record.longName);
-                    System.Diagnostics.Trace.TraceInformation(record.description);
-
-
-                    // Execute the insert operation.
                     table.Execute(insertOperation);
-                    System.Diagnostics.Trace.TraceInformation("1234");
-
                     await context.PostAsync("Great! Learnt something new today!");
                 }
                 catch (StorageException ex)
@@ -252,8 +211,7 @@ namespace Microsoft.Bot.Sample.QnABot
         }
     }
 
-// For more information about this template visit http://aka.ms/azurebots-csharp-qnamaker
-[Serializable]
+    [Serializable]
     public class BasicQnAMakerDialog : QnAMakerDialog
     {
         // Go to https://qnamaker.ai and feed data, train & publish your QnA Knowledgebase.        
